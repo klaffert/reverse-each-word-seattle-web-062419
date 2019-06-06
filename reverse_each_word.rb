c9aaeafff33d35aff8315c57d17f19504eb3f3cd
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 16:46:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ string = "Hello there, and how are you?"
 
   def reverse_each_word(string)
     split_string = string.split(" ")
-    split.string.collect do |word|
+    split_string.collect do |word|
       word.reverse!
     end
     split_string.join(' ')
