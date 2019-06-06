4337100ff3ebf9225e282e49538122e194f40082
Kelliann Lafferty <kell10192@gmail.com>
2019-06-06 16:40:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@ string = "Hello there, and how are you?"
 def reverse_each_word(string)
   split_string = string.split(" ") # converts string to array; ["Hello", "there," "and", "how", "are", "you"]
   split_string.each do |word| # for each word in the split_string array do...
-    word.reverse! # reverse the word for the current array in place
+    word.reverse! # reverse the word for the current array in place ["olleH", ",ereht", "dna", "woh", "era", "?uoy"]
   end 
+  split_string.join(' ')
 end
   
   
